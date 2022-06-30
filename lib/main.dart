f38d9cc6ec9de62f0b1ba019c48024afd92f8f23
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-06-29 23:20:07 -0300

    Fix # on url path

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:phferreira/home/home_page.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
+
   runApp(
     MaterialApp(
       title: 'phferreira',
