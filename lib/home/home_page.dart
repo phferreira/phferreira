7b93b73c33af450cbcb65b928e6d073ac2ff0876
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-06-30 22:46:44 -0300

    Included menu on home_page - Add new widget to control itens of menu

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'widgets/menu_button_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MenuButtonWidget(title: 'About', onPressed: () {}),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            MenuButtonWidget(title: 'Experience', onPressed: () {}),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            MenuButtonWidget(title: 'Work', onPressed: () {}),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            MenuButtonWidget(title: 'Contact', onPressed: () {}),
+          ],
+        ),
       ),
       body: const Center(
         child: Text(
---
+++ lib/home/widgets/menu_button_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MenuButtonWidget extends StatefulWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const MenuButtonWidget({super.key, required this.title, required this.onPressed});
+
+  @override
+  _MenuButtonWidgetState createState() => _MenuButtonWidgetState();
+}
+
+class _MenuButtonWidgetState extends State<MenuButtonWidget> {
+  bool isHover = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: widget.onPressed,
+      child: Text(
+        widget.title,
+        style: TextStyle(
+          color: isHover ? Colors.deepPurple : Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      onHover: (hover) {
+        isHover = hover;
+        setState(() {});
+      },
+    );
+  }
+}
