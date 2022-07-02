b781276ff87e7961cc17fdeeb48035edc7629bb6
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-07-02 14:21:03 -0300

    Include default pages

---
+++ lib/about/about_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlue,
+    );
+  }
+}
