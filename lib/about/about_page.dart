23382272f2f0519c3ef13bd6dedb61232d6aa15f
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-07-04 22:51:29 -0300

    Included about page - Use google_fonts - Name on center - Include github,linkedin and facebook icons on bottom

--- lib/about/about_page.dart
+++ lib/about/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -6,7 +7,50 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.lightBlue,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 300,
+          ),
+          FittedBox(
+            child: Text(
+              'Paulo Henrique Ferreira',
+              style: GoogleFonts.pacifico(
+                fontSize: 64,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 300,
+          ),
+          SizedBox(
+            width: 700,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/github_logo.png'),
+                  tooltip: 'GitHub',
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/linkedin_logo.png'),
+                  tooltip: 'LinkedIn',
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/icons/facebook_logo.png'),
+                  tooltip: 'Facebook',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
