d1910af14f93714e1f6b1a38734bbfae4dfd4e62
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-07-11 22:26:49 -0300

    Change menu - Pass to use NavigationRail insted AppBar - Refactoring

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -3,7 +3,6 @@ import 'package:phferreira/about/about_page.dart';
 import 'package:phferreira/contact/contact_page.dart';
 import 'package:phferreira/experience/experience_page.dart';
 import 'package:phferreira/work/work_page.dart';
-import 'widgets/menu_button_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,13 +12,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int page = 0;
-  late PageController pageController;
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
 
   @override
-  void initState() {
-    super.initState();
-    pageController = PageController(initialPage: page);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          NavigationRail(
+            onDestinationSelected: (index) {
+              setState(() {
+                _selectedIndex = index;
+                pageController.animateToPage(
+                  _selectedIndex,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.ease,
+                );
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            destinations: const [
+              NavigationRailDestination(icon: Icon(Icons.person), label: Text('About')),
+              NavigationRailDestination(icon: Icon(Icons.computer), label: Text('Experience')),
+              NavigationRailDestination(icon: Icon(Icons.work), label: Text('Work')),
+              NavigationRailDestination(icon: Icon(Icons.contacts), label: Text('Contact')),
+            ],
+            selectedIndex: _selectedIndex,
+            backgroundColor: Colors.black12,
+          ),
+          Expanded(
+            child: Scrollbar(
+              thumbVisibility: true,
+              child: PageView(
+                scrollDirection: Axis.vertical,
+                controller: pageController,
+                pageSnapping: false,
+                children: const [
+                  AboutPage(),
+                  ExperiencePage(),
+                  WorkPage(),
+                  ContactPage(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -29,52 +70,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MenuButtonWidget(
-              title: 'About',
-              page: 0,
-              controller: pageController,
-            ),
-            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-            MenuButtonWidget(
-              title: 'Experience',
-              page: 1,
-              controller: pageController,
-            ),
-            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-            MenuButtonWidget(
-              title: 'Work',
-              page: 2,
-              controller: pageController,
-            ),
-            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-            MenuButtonWidget(
-              title: 'Contact',
-              page: 3,
-              controller: pageController,
-            ),
-          ],
-        ),
-      ),
-      body: Scrollbar(
-        thumbVisibility: true,
-        child: PageView(
-          scrollDirection: Axis.vertical,
-          controller: pageController,
-          pageSnapping: false,
-          children: const [
-            AboutPage(),
-            ExperiencePage(),
-            WorkPage(),
-            ContactPage(),
-          ],
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
   }
 }
