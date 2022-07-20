f2c76390f955891e2b23a54080643f69a399b24b
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2022-07-19 21:44:13 -0300

    Change to use SocialMediaWidget

--- lib/about/about_page.dart
+++ lib/about/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:phferreira/about/widgets/social_media_widget.dart';
 import 'package:phferreira/about/widgets/word_animation_widget.dart';
 
 class AboutPage extends StatelessWidget {
@@ -11,57 +12,14 @@ class AboutPage extends StatelessWidget {
       padding: const EdgeInsets.all(24),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Expanded(
+        children: const [
+          Expanded(
             flex: 8,
             child: WordAnimationWidget(),
           ),
           Expanded(
             flex: 2,
-            child: SizedBox(
-              width: 700,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/images/icons/github_logo_256_black.png',
-                      filterQuality: FilterQuality.medium,
-                    ),
-                    tooltip: 'GitHub',
-                    iconSize: 32,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/images/icons/linkedin_logo_256_black.png',
-                      filterQuality: FilterQuality.medium,
-                    ),
-                    tooltip: 'LinkedIn',
-                    iconSize: 32,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/images/icons/facebook_logo_256_black.png',
-                      filterQuality: FilterQuality.medium,
-                    ),
-                    tooltip: 'Facebook',
-                    iconSize: 32,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/images/icons/discord_logo_256_black.png',
-                      filterQuality: FilterQuality.medium,
-                    ),
-                    tooltip: 'Discord',
-                    iconSize: 32,
-                  ),
-                ],
-              ),
-            ),
+            child: SocialMediaWidget(),
           ),
         ],
       ),
